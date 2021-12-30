a6f66acc65ff9367941f0cb76125e168d5a7915c
milan.suk <milan.suk@kiwi.com>
2021-12-30 14:04:19 +0100

    tmp: use reader transformer

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,34 +1,36 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Lib
-    ( fetchUserPassword
-    , MockUserRepository (MockUserRepository)
-    , User (..)
-    , listUserPasswords
-    ) where
+  ( fetchUserPassword,
+    MockUserRepository (MockUserRepository),
+    User (..),
+    listUserPasswords,
+  )
+where
 
-import           Control.Monad.Reader
+import Control.Monad.Trans.Maybe (MaybeT, hoistMaybe)
+import Control.Monad.Trans.Reader (ReaderT, ask)
+import Control.Monad (liftM)
 
-data User = User { username :: String
-                 , password :: String
-                 , age      :: Int
-                 }
+data User = User
+  { username :: String,
+    password :: String,
+    age :: Int
+  }
 
 class UserRepository r where
-  getByUserName :: r -> String -> IO (Maybe User)
+  getByUserName :: r -> String -> MaybeT IO User
 
 data MockUserRepository = MockUserRepository
 
 instance UserRepository MockUserRepository where
-  getByUserName _ userName = (return . return) $ User userName (userName ++ "password") undefined
+  getByUserName _ userName = hoistMaybe . return $ User userName (userName ++ "password") undefined
 
 fetchUserPassword :: UserRepository r => String -> ReaderT r IO (Maybe String)
 fetchUserPassword userName = do
   userRepository <- ask
-  maybeUser :: Maybe User <- lift $ getByUserName userRepository userName
-  return $ case maybeUser of
-    Just user -> Just $ password user
-    Nothing   -> Nothing
+  user <- getByUserName userRepository userName
+  return . return $ password user
 
 listUserPasswords :: UserRepository r => [String] -> ReaderT r IO [Maybe String]
 listUserPasswords = mapM fetchUserPassword
